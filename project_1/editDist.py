6a2d7cec55febd2a5326d39f7140cc3a42a90053
Patrick El-Hage <patrick.elhage@gmail.com>
2016-02-11 22:52:50 -0500

    Set up basic file reads

---
+++ project_1/editDist.py
@@ -0,0 +1,14 @@
+import sys
+
+file_1 = open(sys.argv[1], 'r').read()
+file_2 = open(sys.argv[2], 'r').read()
+
+print file_1
+print file_2
+
+
+### NOTES TAKEN TO COMPLETE THE PROJECT ###
+'''
+
+
+'''
